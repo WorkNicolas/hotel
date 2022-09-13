dbde12bd3e582ed11e5b445bccc5e77ca99fc40c
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-13 22:03:18 +0800

    Feat: PaymentComponent Overloaded constructors

--- src/payment/PaymentComponent.java
+++ src/payment/PaymentComponent.java
@@ -1,43 +1,69 @@
 package payment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 import javax.swing.*;
 
 /**
- * Creates an InputDialog with a listener attached
+ * Creates an InputDialog with a listener attached.
+ * @apiNote a callback `onSuccess` receives the account name.
  */
 public class PaymentComponent implements ActionListener {
     protected JFrame parent;
     protected String portalName;
-    protected Consumer<String> onSuccess;
-    /**
-     * @param parent
-     * @param onSuccess - Receives the account name entered by the user
-     */
-    public PaymentComponent(JFrame parent, Consumer<String> onSuccess) {
-        this.parent = parent;
-        portalName = "";
-        this.onSuccess = onSuccess;
+    public String getPortalName() {
+        return portalName;
     }
 
     public void setPortalName(String portalName) {
         this.portalName = portalName;
     }
 
+    protected float amount;
+    public float getAmount() {
+        return amount;
+    }
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    protected Consumer<String> consumer;
+    public Consumer<String> getConsumer() {
+        return consumer;
+    }
+    public void setConsumer(Consumer<String> consumer) {
+        this.consumer = consumer;
+    }
+    /**
+     * @param parent
+     * @param consumer receives the valid account name entered by the user.
+     */
+    public PaymentComponent(JFrame parent, Consumer<String> consumer) {
+        this(parent, consumer, 0);
+    }
+
+    public PaymentComponent(JFrame parent, Consumer<String> consumer, float amount) {
+        this.parent = parent;
+        this.amount = amount;
+        this.consumer = consumer;
+        this.portalName = "";
+    }
+
+    public PaymentComponent(JFrame parent, Consumer<String> consumer, float amount, String portaleName) {
+        this(parent, consumer, amount);
+        this.portalName = portaleName;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-        String accountName = "";
-        try {
-            accountName = JOptionPane.showInputDialog(
+        Optional<String> maybeAccountName = Optional.ofNullable(null);
+            maybeAccountName = Optional.ofNullable(
+            JOptionPane.showInputDialog(
                 parent, 
                 "Amount: TODO\nEnter account:", 
                 "Payment Portal: " + portalName, 
-                JOptionPane.QUESTION_MESSAGE);
-        } catch (Exception ex) {
-            return; //PASS
-        }
-        onSuccess.accept(accountName);
+                JOptionPane.QUESTION_MESSAGE));
+        maybeAccountName.ifPresent(consumer);
     }
 }
