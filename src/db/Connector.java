111369cdc350fff4c12f9db04cbb8f5ad0c18321
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-13 14:45:48 +0800

    Feat: Dynamic db access using env variables

--- src/db/Connector.java
+++ src/db/Connector.java
@@ -7,19 +7,19 @@ import java.sql.SQLException;
  * Helper class for communicating with the assigned DB
  */
 public class Connector {
-    protected static final String DB_HOST = "localhost";
-    protected static final String DB_USER = "hotel-manager";
-    protected static final String DB_PASS = "hotel-manager";
-    protected static final String DB_NAME = "hotel";
+    protected static final String DB_HOST = System.getenv("DB_HOST");
+    protected static final String DB_USER = System.getenv("DB_USER");
+    protected static final String DB_PASS = System.getenv("DB_PASS");
+    protected static final String DB_NAME = System.getenv("DB_NAME");
+    protected static final String URL = "jdbc:mysql://" + DB_HOST + "/" + DB_NAME;
     /** 
      * {@summary Creates a connection using the Connector's credentials} 
      */
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(
-            "jdbc:mysql://" 
-            + DB_HOST + "/" 
-            +DB_NAME, 
-            DB_USER, DB_PASS
+            URL,
+            DB_USER, 
+            DB_PASS
         );
     }
 }
