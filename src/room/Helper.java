6926e6186cc0a89d239d2b6949721a43997b1d85
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-14 13:38:31 +0800

    Feat: url->ImageIcon helper

---
+++ src/room/Helper.java
@@ -0,0 +1,23 @@
+package room;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+public class Helper {
+    public static ImageIcon createImageIcon(Color color, int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setPaint(color);
+        graphics.fillRect (0, 0, width, height);
+        return new ImageIcon(image);
+    }
+
+    public static ImageIcon imageIconFromURL(String urlStr) throws IOException {
+        return new ImageIcon(
+                new URL(urlStr));
+    }
+}
