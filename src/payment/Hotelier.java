4e07d5ffec45ba4bc8705b658fa820ba951ea325
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-14 14:02:33 +0800

    Feat: Reservation: table creation code

--- src/payment/Hotelier.java
+++ src/payment/Hotelier.java
@@ -12,7 +12,9 @@ import room.Info;
 
 public class Hotelier extends Connector{
     protected static final String TABLE_NAME = "reservations";
-
+    public Hotelier() throws SQLException {
+        Connector.executeSQL("CREATE TABLE IF NOT EXISTS `reservations` ( `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key', `startsAt` date NOT NULL, `length` tinyint NOT NULL, `room_id` int NOT NULL, `tenant_id` int NOT NULL, PRIMARY KEY (`id`), KEY `room_id` (`room_id`), KEY `tenant_id` (`tenant_id`), CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`), CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`tenant_id`) REFERENCES `users` (`id`) )");
+    }
     /**
      * @return n > 0 it is the newly created record's id. Otherwise it failed
      */
