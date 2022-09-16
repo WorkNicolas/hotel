3b9ebe59075224a2f923b5f784166ea35a3ed67c
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-16 22:00:02 +0800

    Feat: Split GUI design from App though the latter  extends it for simplicity.

--- src/App.java
+++ src/App.java
@@ -1,14 +1,12 @@
-import java.awt.EventQueue;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
+import java.awt.EventQueue;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-import auth.LoginForm;
 import auth.LoginListener;
 import auth.LoginObserver;
 import auth.User;
@@ -17,273 +15,173 @@ import payment.PaymentComponent;
 import reservation.Hotelier;
 import reservation.Reservation;
 import reservation.ReservationState;
-import reservation.ReservationTicketView;
 import room.Manager;
-import room.RoomListing;
-import service.RoomServiceView;
-import service.Supplier;
 
 /**
  * @author Jean Carlo Molina San Juan
  */
-public class App extends JFrame implements Subscriber<State> {
-	private JMenuBar navbar = new JMenuBar();
-	private JMenu menuUser = new JMenu("User");
-	private JMenuItem uLogin = new JMenuItem("Login");
-	private JMenuItem uRegister = new JMenuItem("Register");
-	private JMenuItem uLogout = new JMenuItem("Logout");
-	private JMenu menuReservation = new JMenu("Reservation");
-	private JMenuItem rInfo = new JMenuItem("Info");
-	private JMenuItem rNew = new JMenuItem("New");
-	private JMenuItem rExtend = new JMenuItem("Extend");
-	private JMenuItem rCancel = new JMenuItem("Cancel");
-	private JMenu menuRoomService = new JMenu("Room Service");
+public class App extends View implements Subscriber<State> {
+    Subscription subscription;
 	private Verifier verifier = new Verifier();
-	private LoginForm loginForm = new LoginForm();
-	private RoomListing roomListing = new RoomListing();
-	private RoomServiceView roomService;
-	private HashMap<State, JPanel> panels = new HashMap<>();
-	private ReservationTicketView ticket;
-	private JPanel active;
-	private Subscription subscription;
-	private Manager manager;
-	private LoginObserver loginObserver = new LoginObserver() {
-
-		/**
-		 * Determines where the user is redirected after login.
-		 */
-		@Override
-		public void onSuccess(User u) {
-			loginForm.clear();
-			ArrayList<Reservation> reservations = Hotelier.getReservations(u);
-			ReservationState rs = Hotelier.getStatus(reservations);
-			switch(rs) {
-				case DONE: case NONE:
-					roomListing.updateEntries(
-						new ArrayList<>(
-							manager.fetchAvailable().values()
-						)
-					);
-					Status.self.submit(State.BROWSE);
-
-					break;
-				case ONGOING:
-					Status.self.submit(State.CHECKEDIN);
-					break;
-				case UPCOMING:
-					Status.self.submit(State.BOOKED);
-					break;
-				default:
-					break;
-			}
-			ReservationStatus.self.submit(rs);
-		}
-
-		@Override
-		public void onFail() {
-			// TODO Auto-generated method stub
-		}
-
-		@Override
-		public void onMaxTries() {
-			Status.self.close();
-		}
-
-		@Override
-		public void onRegister() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void onGuest() {
-			// TODO Auto-generated method stub
-			
-		}
-	};
-
-	private LoginListener loginListener = new LoginListener(loginForm, 10, verifier, loginObserver);
-
-	public App() throws SQLException {
-		setTitle("Hotel El San Juan");
-		{ // MAINTAIN CALL ORDER
-			initMenu();
-			manager = new Manager();
-			initComponents();
-			setExtendedState(JFrame.MAXIMIZED_BOTH); // MAXIMIZE
-			pack(); // FORCE RESIZE
-			setVisible(true);
-			setMinimumSize(getSize()); // SAVE THE NEW DIMENSIONS
-			for (JPanel p : panels.values()) {
-				if (p != null) {
-					p.setPreferredSize(getSize());
-					p.setMinimumSize(getMinimumSize());
-				}
-			}
-		}
-
-		setLocationRelativeTo(getOwner());
-		Status.self.subscribe(this);
-	}
-
-	private void initMenu() {
-		uLogout.addActionListener(e -> {
-			Status.self.submit(State.auth);
-			ReservationStatus.self.submit(ReservationState.NONE);
-		});
-
-		var order = new JMenuItem("Order");
-		order.addActionListener(e -> {
-			Status.self.submit(State.CHECKEDIN);
-		});
-		rInfo.addActionListener(e -> {
-			activate(ticket);
-			//DO NOT CHANGE Status
-		});
-
-		order.addActionListener(e -> {
-			activate(roomService);
-		});
-		menuUser.add(uLogin);
-		menuUser.add(uRegister);
-		menuUser.add(uLogout);
-		navbar.add(menuUser);
-		menuReservation.add(rInfo);
-		menuReservation.add(rNew);
-		menuReservation.add(rExtend);
-		menuReservation.add(rCancel);
-		menuRoomService.add(order);
-		navbar.add(menuReservation);
-		navbar.add(menuRoomService);
-		setJMenuBar(navbar);
-
-		Subscriber<ReservationState> reservationStatusObserver = new Subscriber<ReservationState>() {
-			private Subscription subscription;
-			@Override
-			public void onSubscribe(Subscription subscription) {
-				this.subscription = subscription;	
-				this.subscription.request(1);
-			}
-
-			@Override
-			public void onNext(ReservationState item) {
-				System.out.println("Item:" + item);
-				switch (item) {
-					case NONE: case DONE:
-						//TODO: For case DONE: Allow viewing past tickets.
-						rNew.setEnabled(true);
-						rInfo.setEnabled(false);
-						rExtend.setEnabled(false);
-						rCancel.setEnabled(false);
-						order.setEnabled(false);
-						break;
-					case ONGOING:
-						rNew.setEnabled(false);
-						rExtend.setEnabled(true);
-						rCancel.setEnabled(true);
-						order.setEnabled(true);
-						rInfo.setEnabled(true);
-						break;
-					case UPCOMING:
-						rNew.setEnabled(false);
-						rCancel.setEnabled(true);
-						order.setEnabled(false);
-						rInfo.setEnabled(true);
-						break;
-					default:
-						//PASS
-						break;
-				}
-				this.subscription.request(1);
-			}
-
-			@Override
-			public void onError(Throwable throwable) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onComplete() {
-				// TODO Auto-generated method stub
-			}
-			
-		};
-		ReservationStatus.self.subscribe(reservationStatusObserver);
-		ReservationStatus.self.submit(ReservationState.NONE);
-	}
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					App a = new App();
-					a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	private void initComponents() {
-		{
-			try {
-				roomService = new RoomServiceView(Supplier.fetchAvailable());
-				roomService.setBorder(new EmptyBorder(5, 5, 5, 5));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-			ticket = new ReservationTicketView();
-			ticket.setBorder(new EmptyBorder(5, 5, 5, 5));
-			panels.put(State.BOOKED, ticket);
-			panels.put(State.CHECKEDIN, roomService);
-			panels.put(State.BROWSE, roomListing);
-			panels.put(State.auth, loginForm);
-		}
-		{ // Setup loginForm
-			add(loginForm);
-			activate(loginForm); // FIRST active
-		}
-		{// Setup roomListing
-			PaymentComponent l = new PaymentComponent(this, accountName -> {
-					JOptionPane.showMessageDialog(App.this, accountName);
-			});
-			l.setPortalName("GCASH");
-			roomListing.buttons.forEach(jButton -> {
-				jButton.addActionListener(l);
-			});
-		}
-	}
-
-	public void activate(JPanel a) {
-		if (a == null)
-			return;
-
-		if (active != null) {
-			// Temporarily remove 'a' from the component hierarchy
-			active.setVisible(false);
-			remove(active);
-		}
-		active = a;
-		add(active);
-		active.setVisible(true);
-		pack();
-	}
-
-	@Override
-	public void onSubscribe(Subscription subscription) {
-		this.subscription = subscription;
+    private Manager roomManager;
+    private PaymentComponent paymentComponent;
+	private LoginListener loginListener;
+    private LoginObserver loginObserver;
+    private Subscriber<ReservationState> reservationObserver;
+    public App() throws SQLException {
+        super();
+        {//MENU
+            uLogout.addActionListener(e -> {
+                Status.self.submit(State.auth);
+                ReservationStatus.self.submit(ReservationState.NONE);
+            });
+    
+            order.addActionListener(e -> {
+                /* if (Status.self.value == State.auth) {
+                    Status.self.submit(State.CHECKEDIN);
+                } */
+    			activate(roomService);
+            });
+            rInfo.addActionListener(e -> {
+                activate(ticketView);
+                //DO NOT CHANGE Status
+            });
+        }
+        {// Setup roomListing
+            roomManager = new Manager();
+			paymentComponent = new PaymentComponent(this, accountName -> {
+                JOptionPane.showMessageDialog(this, accountName);
+                paymentComponent.setPortalName("GCASH");
+                roomListingView.buttons.forEach(jButton -> {
+                    jButton.addActionListener(paymentComponent);
+                });
+            });
+        }
+        {//Login related
+           loginObserver = new LoginObserver() {
+                /**
+                 * Determines where the user is redirected after login.
+                 */
+                @Override
+                public void onSuccess(User u) {
+                    loginForm.clear();
+                    ArrayList<Reservation> reservations = Hotelier.getReservations(u);
+                    ReservationState rs = Hotelier.getStatus(reservations);
+                    switch(rs) {
+                        case DONE: case NONE:
+                            roomListingView.updateEntries(
+                                new ArrayList<>(
+                                    roomManager.fetchAvailable().values()
+                                )
+                            );
+                            Status.self.submit(State.BROWSE);
+                            break;
+                        case ONGOING:
+                            Status.self.submit(State.CHECKEDIN);
+                            break;
+                        case UPCOMING:
+                            Status.self.submit(State.BOOKED);
+                            break;
+                        default:
+                            break;
+                    }
+                    ReservationStatus.self.submit(rs);
+                }
+        
+                @Override
+                public void onFail() {
+                    // TODO Auto-generated method stub
+                }
+        
+                @Override
+                public void onMaxTries() {
+                    Status.self.close();
+                }
+        
+                @Override
+                public void onRegister() {
+                    // TODO Auto-generated method stub
+                    
+                }
+        
+                @Override
+                public void onGuest() {
+                    // TODO Auto-generated method stub
+                    
+                }
+            };
+            loginListener = new LoginListener(loginForm, 10, verifier, loginObserver);
+        }
+        {//RESERVATION & MENU
+            reservationObserver = new Subscriber<ReservationState>() {
+                private Subscription subscription;
+                @Override
+                public void onSubscribe(Subscription subscription) {
+                    this.subscription = subscription;	
+                    this.subscription.request(1);
+                }
+    
+                @Override
+                public void onNext(ReservationState item) {
+                    System.out.println("Item:" + item);
+                    switch (item) {
+                        case NONE: case DONE:
+                            //TODO: For case DONE: Allow viewing past tickets.
+                            rNew.setEnabled(true);
+                            rInfo.setEnabled(false);
+                            rExtend.setEnabled(false);
+                            rCancel.setEnabled(false);
+                            order.setEnabled(false);
+                            break;
+                        case ONGOING:
+                            rNew.setEnabled(false);
+                            rExtend.setEnabled(true);
+                            rCancel.setEnabled(true);
+                            order.setEnabled(true);
+                            rInfo.setEnabled(true);
+                            break;
+                        case UPCOMING:
+                            rNew.setEnabled(false);
+                            rCancel.setEnabled(true);
+                            order.setEnabled(false);
+                            rInfo.setEnabled(true);
+                            break;
+                        default:
+                            //PASS
+                            break;
+                    }
+                    this.subscription.request(1);
+                }
+    
+                @Override
+                public void onError(Throwable throwable) {
+                    // TODO Auto-generated method stub
+                    
+                }
+    
+                @Override
+                public void onComplete() {
+                    // TODO Auto-generated method stub
+                }
+                
+            };
+        }
+        {//SUBSCRIPTIONS
+            ReservationStatus.self.subscribe(reservationObserver);
+            ReservationStatus.self.submit(ReservationState.NONE);
+            Status.self.subscribe(this);
+        }
+    }
+    @Override
+	public void onSubscribe(Subscription s) {
+		this.subscription = s;
 		this.subscription.request(1);
 	}
 
 	@Override
-	public void onNext(State item) {
-		activate(
-				panels.get(item));
-		this.subscription.request(1);
+	public void onNext(State s) {
+		activate(s);
+		subscription.request(1);
 	}
 
 	@Override
@@ -297,4 +195,19 @@ public class App extends JFrame implements Subscriber<State> {
 		JOptionPane.showMessageDialog(this, "App will now exit.", "Max login attempts", JOptionPane.WARNING_MESSAGE);
 		System.exit(0); // FORCE SHUTDOWN
 	}
+
+    public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					App a  = new App();
+					a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 }
---
+++ src/View.java
@@ -0,0 +1,117 @@
+import java.util.HashMap;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import auth.LoginForm;
+import reservation.ReservationTicketView;
+import room.RoomListing;
+import service.RoomServiceView;
+import service.Supplier;
+
+/**
+ * @author Jean Carlo Molina San Juan
+ */
+public class View extends JFrame {
+	protected JMenuBar navbar = new JMenuBar();
+	protected JMenu menuUser = new JMenu("User");
+	protected JMenuItem uLogin = new JMenuItem("Login");
+	protected JMenuItem uRegister = new JMenuItem("Register");
+	protected JMenuItem uLogout = new JMenuItem("Logout");
+	protected JMenu menuReservation = new JMenu("Reservation");
+	protected JMenuItem rInfo = new JMenuItem("Info");
+	protected JMenuItem rNew = new JMenuItem("New");
+	protected JMenuItem rExtend = new JMenuItem("Extend");
+	protected JMenuItem rCancel = new JMenuItem("Cancel");
+	protected JMenu menuRoomService = new JMenu("Room Service");
+	protected LoginForm loginForm = new LoginForm();
+	protected RoomListing roomListingView = new RoomListing();
+	protected JMenuItem order = new JMenuItem("Order");
+	protected RoomServiceView roomService;
+	protected HashMap<State, JPanel> panels = new HashMap<>();
+	protected ReservationTicketView ticketView;
+	protected JPanel active;
+
+	public View() {
+		setTitle("Hotel El San Juan");
+		{ // MAINTAIN CALL ORDER
+			initMenu();
+			initComponents();
+			setExtendedState(JFrame.MAXIMIZED_BOTH); // MAXIMIZE
+			pack(); // FORCE RESIZE
+			setVisible(true);
+			setMinimumSize(getSize()); // SAVE THE NEW DIMENSIONS
+			for (JPanel p : panels.values()) {
+				if (p != null) {
+					p.setPreferredSize(getSize());
+					p.setMinimumSize(getMinimumSize());
+				}
+			}
+		}
+
+		setLocationRelativeTo(getOwner());
+	}
+
+	private void initMenu() {
+		menuUser.add(uLogin);
+		menuUser.add(uRegister);
+		menuUser.add(uLogout);
+		navbar.add(menuUser);
+		menuReservation.add(rInfo);
+		menuReservation.add(rNew);
+		menuReservation.add(rExtend);
+		menuReservation.add(rCancel);
+		menuRoomService.add(order);
+		navbar.add(menuReservation);
+		navbar.add(menuRoomService);
+		setJMenuBar(navbar);
+	}
+
+	/**
+	 * @implNote For all State value, a JPanel must be defined in this method.
+	 */
+	private void initComponents() {
+		{
+			try {
+				roomService = new RoomServiceView(Supplier.fetchAvailable());
+				//TODO: defer RoomServiceView setting items.
+				roomService.setBorder(new EmptyBorder(5, 5, 5, 5));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			ticketView = new ReservationTicketView();
+			ticketView.setBorder(new EmptyBorder(5, 5, 5, 5));
+			panels.put(State.BOOKED, ticketView);
+			panels.put(State.CHECKEDIN, roomService);
+			panels.put(State.BROWSE, roomListingView);
+			panels.put(State.auth, loginForm);
+		}
+		{ // Setup loginForm
+			add(loginForm);
+			activate(loginForm); // FIRST active
+		}
+	}
+
+	/**
+	 * Changes the visible JPanel
+	 */
+	public void activate(JPanel a) {
+		if (a == null)
+			return;
+
+		if (active != null) {
+			// Temporarily remove 'a' from the component hierarchy
+			active.setVisible(false);
+			remove(active);
+		}
+		active = a;
+		add(active);
+		active.setVisible(true);
+		pack();
+	}
+
+	public void activate(State s) {
+		activate(panels.get(s));
+	}
+}
