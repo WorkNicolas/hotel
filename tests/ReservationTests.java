8d79df7b30aa703efeb464d063ce255eeff76063
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-17 07:59:46 +0800

    Fix: tests for reservations

--- tests/ReservationTests.java
+++ tests/ReservationTests.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.function.Consumer;
 
 import reservation.ContactInfo;
@@ -22,24 +23,31 @@ import service.Amenity;
 
 public class ReservationTests {
 
+    public Stay genStay() {
+        Date start = Date.valueOf(LocalDate.of(2002, Month.SEPTEMBER, 14));
+        Date end = Date.valueOf(LocalDate.of(2002, Month.SEPTEMBER, 18));
+        return new Stay(start, end);
+    }
     @Test
     public void shouldCommitAndCanel() throws FileNotFoundException, SQLException {
         Manager m = new Manager(1);
         assertEquals(1, m.fetchAvailable().size());
+        var stay = genStay();
+
         m.infos.values().forEach(new Consumer<RawInfo>() {
 
             @Override
             public void accept(RawInfo raw) {
                 Reservation r;
+                
                 try {
                     r = new Reservation(
                             0,
                             new ContactInfo("Jean Carlo M. San Juan",
                                     "jc@sj", ""),
                             new Info(raw),
-                            new Stay(
-                                    Date.valueOf(LocalDate.now()),
-                                    5));
+                            stay
+                    );
                 } catch (Exception e) {
                     e.printStackTrace();
                     return;
@@ -65,7 +73,7 @@ public class ReservationTests {
 
     @Test
     public void correctReceipt() {
-        var r = new Reservation(0, new ContactInfo("Test", "example@email", "1234 NW Bobcat Lane, St. Robert"), new Info(0, Type.BASIC, 1, "https://i0.wp.com/theluxurytravelexpert.com/wp-content/uploads/2014/03/trump-hotel-chicago-illinois-usa.jpg", 100), new Stay(Date.valueOf(LocalDate.now()), 1));
+        var r = new Reservation(0, new ContactInfo("Test", "example@email", "1234 NW Bobcat Lane, St. Robert"), new Info(0, Type.BASIC, 1, "https://i0.wp.com/theluxurytravelexpert.com/wp-content/uploads/2014/03/trump-hotel-chicago-illinois-usa.jpg", 100, "Test roo"), genStay());
         r.put(new Amenity(0, "Anything",service.Type.THING , 10, 10, 100));
     }
 }
