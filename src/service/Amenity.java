6bfc6f6e4ce288516d0c10256fa4238ca1cea99f
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-16 09:41:45 +0800

    Feat: add amount field to Amenity

--- src/service/Amenity.java
+++ src/service/Amenity.java
@@ -2,18 +2,38 @@ package service;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class Amenity {
     protected final int id;
     protected final String name;
     protected final Type type;
-    public Amenity(ResultSet rs) throws SQLException {
-        this.id = rs.getInt("id");
-        this.name = rs.getString("name");
-        this.supply = rs.getInt("supply");
-        this.type = Type.valueOf(rs.getString("type"));
-        this.price = rs.getInt("price");
+    protected final float price;
+    protected int supply;
+    protected int amount;
+
+    public int getAmount() {
+        return amount;
     }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Amenity(ResultSet rs) throws SQLException {
+        this(
+            rs.getInt("id"),
+            rs.getString("name"),
+            Type.valueOf(rs.getString("type")),
+            Optional.ofNullable(rs.getInt("amount")).orElse(0),
+            rs.getInt("price"),
+            rs.getInt("supply"));
+    }
+
     public int getId() {
         return id;
     }
@@ -30,15 +50,17 @@ public class Amenity {
         this.supply = supply;
     }
 
-    protected final float price;
-    protected int supply;
-
     public Amenity(int id, String name, float price, int supply, Type type) {
+        this(id, name, type, 0, price, supply);
+    }
+
+    public Amenity(int id, String name, Type type, int amount, float price, int supply) {
         this.id = id;
         this.name = name;
+        this.type = type;
+        this.amount = amount;
         this.price = price;
         this.supply = supply;
-        this.type = type;
     }
 
     public boolean reduce(int amount) {
