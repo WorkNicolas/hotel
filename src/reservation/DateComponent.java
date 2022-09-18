25a63508ab40f6b424398014df439faf449f1be1
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-18 20:44:39 +0800

    FIX: Commit DateComponent

---
+++ src/reservation/DateComponent.java
@@ -0,0 +1,30 @@
+package reservation;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+import java.awt.Font;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+public class  DateComponent extends SpinnerDateModel {
+    public JSpinner ui;
+    private final String pattern = "yyyy-MM-dd";
+    public DateComponent() {
+        ui = new JSpinner(this);
+        ui.setFont(new Font("Consolas", Font.PLAIN, 24));
+        var e = new JSpinner.DateEditor(ui,pattern);
+        ui.setEditor(e);
+    }
+
+    /**
+     * @return an instance of java.sql.Date
+     */
+    public Date getSQLDate() {
+        return Date.valueOf(
+            new SimpleDateFormat(pattern)
+            .format(getDate())
+        );
+    }
+    public static void main(String[] args) {
+        var c = new DateComponent();
+        System.out.println(c.getSQLDate());
+    }
+}
