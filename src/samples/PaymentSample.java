09da42d947d24fbe5f8ba70b54c34288cfa6f7cb
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-18 23:29:06 +0800

    Feat: payment sample

---
+++ src/samples/PaymentSample.java
@@ -0,0 +1,32 @@
+package samples;
+import javax.swing.JDialog;
+
+import payment.Discount;
+import payment.PaymentPanel;
+
+public class PaymentSample {
+    public static void main(String[] args) {
+        try {
+            var p = new PaymentPanel<String>(
+                100f,
+                new String[] {
+                    "Test", 
+                    "Test 1"
+                }, 
+                new Discount[] {
+                    new Discount("NONE", 0f),
+                    new Discount("SENIOR CITIZEN", 0.2f),
+                    new Discount("PERSON WITH DISABILITY", 0.2f),
+                    new Discount("VOUCHER", 0.05f),
+                }
+            );
+            var d = new JDialog();
+            d.add(p);
+            d.pack();
+            d.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // TODO: handle exception
+        }
+    }
+}
