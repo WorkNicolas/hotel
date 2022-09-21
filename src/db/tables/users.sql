56dc4f111bef7c069bfbaab99a769c5f0db4d8ec
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-09-21 09:17:05 +0800

    Fix: sql

--- src/db/tables/users.sql
+++ src/db/tables/users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(255) NOT NULL,
   `phrase` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `contact` varchar(11) NOT NULL,
+  `contact` varchar(11) NOT NULL
 );
